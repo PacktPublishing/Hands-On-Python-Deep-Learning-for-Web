16857095663a2ac24f1a3df920ccd208db48d41f
xprilion <xprilion@gmail.com>
2019-10-21 00:41:00 +0530

    Chapter 11

---
+++ Chapter11/app/main.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify, render_template
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+
+np.random.seed(5)
+
+df = pd.read_csv("../data/heart.csv")
+
+X = df.drop("target",axis=1)
+y = df["target"]
+
+X = StandardScaler().fit_transform(X)
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.20,random_state=0)
+
+clf = MLPClassifier(max_iter=200)
+
+for i in range(100):
+    xt = X_train[i].reshape(1, -1)
+    yt = y_train.values[[i]]
+    clf = clf.partial_fit(xt, yt, classes=[0,1])
+    if i > 0 and i % 25 == 0 or i == len(X_train) - 1:
+        score = clf.score(X_test, y_test)
+        print("Iters ", i, ": ", score)
+
+score = clf.score(X_test, y_test)
+
+app = Flask(__name__)
+
+start_at = 100
+
+@app.route('/train_batch', methods=['GET', 'POST'])
+def train_batch():
+    global start_at, clf, X_train, y_train, X_test, y_test, score
+    for i in range(start_at, min(start_at+25, len(X_train))):
+        xt = X_train[i].reshape(1, -1)
+        yt = y_train.values[[i]]
+        clf = clf.partial_fit(xt, yt, classes=[0,1])
+
+    score = clf.score(X_test, y_test)
+
+    start_at += 25
+
+    response = {'result': float(round(score, 5)), 'remaining': len(X_train) - start_at}
+
+    return jsonify(response)
+
+@app.route('/reset', methods=['GET', 'POST'])
+def reset():
+    global start_at, clf, X_train, y_train, X_test, y_test, score
+    start_at = 0
+    del clf
+    clf = MLPClassifier(max_iter=200)
+    for i in range(start_at, start_at+1):
+        xt = X_train[i].reshape(1, -1)
+        yt = y_train.values[[i]]
+        clf = clf.partial_fit(xt, yt, classes=[0,1])
+
+    score = clf.score(X_test, y_test)
+
+    start_at += 1
+
+    response = {'result': float(round(score, 5)), 'remaining': len(X_train) - start_at}
+
+    return jsonify(response)
+
+@app.route('/')
+def index():
+    global score, X_train
+    rem = (len(X_train) - start_at) > 0
+
+    return render_template("index.html", score=round(score, 5), remain = rem)
+
+if __name__ == '__main__':
+    app.run(port=9000, debug=True)
