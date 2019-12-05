111b206a549a52f05192d510d4be38016c3c3c71
xprilion <xprilion@gmail.com>
2019-12-06 02:57:32 +0530

    abcd

--- Chapter12/ordersui/ordersui/settings.py
+++ Chapter12/ordersui/ordersui/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
